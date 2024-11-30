36e429f39715b22e8727a090760fd3ff15ee362a
Sumit Kant <sumit.kant9@live.com>
2024-11-30 22:41:31 +0530

    extended hindu premium to 2 pages

--- main.py
+++ main.py
@@ -166,19 +166,34 @@ elif menu == 'SEARCH':
 elif menu == 'HINDU PREMIUM':
 	
 	st.image('https://www.thehindu.com/theme/images/th-online/thehindu-logo.svg')
-	r =  requests.get('https://www.thehindu.com/premium/')
-	soup = BeautifulSoup(r.content,  "html.parser")
-	result = soup.find_all('div', class_='result')[0]
-	divs = result.find_all('div', class_='element row-element')
-	links = []
 
-	for div in divs:
-	    temp_dict = {
-	        'title': div.find('h3').find('a').text.strip(),
-	        'href': div.find('h3').find('a')['href'],
-	        'img': div.find('div', class_='picture').find('img')['data-original']
-	    }
-	    links.append(temp_dict)
+	@st.cache_data
+	def scrapePages():
+		links = []
+		for page in [1,2]:
+		    r =  requests.get(f'https://www.thehindu.com/premium/?page={page}')
+		    soup = BeautifulSoup(r.content,  "html.parser")
+		    result = soup.find_all('div', class_='result')[0]
+		    divs = result.find_all('div', class_='element row-element')
+		    maindiv = soup.find_all('div', class_='element main-row-element')[0]
+		    equalrow = soup.find_all('div', class_='row equal-height')[0].find_all('div', class_='element')
+
+		    for div in [maindiv] + equalrow + divs:
+		        try:
+		            img = div.find('div', class_='picture').find('img')['data-src-template']
+		        except:
+		            img = None
+		        temp_dict = {
+		            'title': div.find('h3').find('a').text.strip(),
+		            'href': div.find('h3').find('a')['href'],
+		            'img': img
+		        }
+		        
+		        if not temp_dict['title'] in [x['title'] for x in links]:
+		        	links.append(temp_dict)
+		return links
+
+	links = scrapePages()
 	
 	@st.dialog('💎', width='large')
 	def loadPremiumArticle(i):
@@ -201,6 +216,9 @@ elif menu == 'HINDU PREMIUM':
 	cols = st.columns(7, gap='small')
 	for i, item in enumerate(links):
 		c = cols[i%len(cols)]
-		img = c.image(item['img'].replace('SQUARE_80','SQUARE_140'), use_container_width=True)
+		try:
+			img = c.image(item['img'].replace('SQUARE_80','SQUARE_140'), use_container_width=True)
+		except:
+			img = c.image('https://www.thehindu.com/theme/images/th-online/thumbnail-rectangle.svg', use_container_width=True)
 		if c.button(f"{item['title']}", use_container_width=True):
 			loadPremiumArticle(item)
