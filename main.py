ee110d4691b4b1ead84bb5fee88710ee617a7ce4
Sumit Kant <sumit.kant9@live.com>
2024-11-30 20:18:18 +0530

    added hindu scrape

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import streamlit as st
 import requests
 import json
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 
 
 st.set_page_config(layout="wide")
 
 apiKey = st.secrets["newsapikey"]
 
-st.title(':bullettrain_front: THE JOHNNY\'S EXPRESS ')
+# st.markdown("<img src='logo.png'>")
+st.image('logo.png', width=200)
+# st.title(':bullettrain_front: THE JOHNNY\'S EXPRESS ')
 
 
 @st.cache_data
@@ -39,7 +42,7 @@ def getGenAI(api):
 sources = getSources(apiKey)
 defaultimg = 'https://images.unsplash.com/photo-1495020689067-958852a7765e?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
-menu  = st.selectbox('Select:', ['HOME','SEARCH'], 0)
+menu  = st.selectbox('Select:', ['HOME','SEARCH','HINDU PREMIUM'], 0)
 
 if menu == 'HOME':
 	topHeadlines = getTopHeadlines(apiKey)
@@ -119,7 +122,7 @@ elif menu == 'SEARCH':
 	
 	c1,c2,c3 = st.columns(3)
 	
-	query = c1.text_input('Search Query :')
+	query = c1.text_input('Topic :', placeholder='Bitcoin')
 	sortBy = c2.selectbox('Sort By :', ['relevancy','popularity','publishedAt'], 1)
 	yesterday = datetime.now() - timedelta(1)
 	fromdate = c3.date_input('FROM :', yesterday)
@@ -127,20 +130,15 @@ elif menu == 'SEARCH':
 	cols = st.columns(10)
 	sources = [x for x in sources if x['country'] in ('us','in')]
 	sources = [x for x in sources if x['category'] in ('science','technology','business','general','health')]
-	# st.write(set([x['category'] for x in sources]))
-	# for i, source in enumerate(sources):
-	# 	cols[i%len(cols)].checkbox(source['name'], value=source['id'])
 
 	if query:
 		@st.cache_data
 		def getQueryArticles(query, sortBy, fromdate, api):
 			u = f"""https://newsapi.org/v2/everything?q="{query.lower().replace(' ','%20')}"&sortBy={sortBy}&from={fromdate}&language=en&apiKey={api}""".replace(' ','%20')
-			# st.write(u)
 			r = requests.get(u)
 			return json.loads(r.text)['articles']
 		articles = getQueryArticles(query, sortBy, fromdate, apiKey)
 		articles = [x for x in articles if x['title'] != "[Removed]"]
-		# st.write(articles)	
 
 		st.title(query.upper())
 
@@ -164,3 +162,45 @@ elif menu == 'SEARCH':
 			    unsafe_allow_html=True
 			)
 			c.divider()
+
+elif menu == 'HINDU PREMIUM':
+	
+	st.image('https://www.thehindu.com/theme/images/th-online/thehindu-logo.svg')
+	r =  requests.get('https://www.thehindu.com/premium/')
+	soup = BeautifulSoup(r.content,  "html.parser")
+	result = soup.find_all('div', class_='result')[0]
+	divs = result.find_all('div', class_='element row-element')
+	links = []
+
+	for div in divs:
+	    temp_dict = {
+	        'title': div.find('h3').find('a').text.strip(),
+	        'href': div.find('h3').find('a')['href'],
+	        'img': div.find('div', class_='picture').find('img')['data-original']
+	    }
+	    links.append(temp_dict)
+	
+	@st.dialog('💎', width='large')
+	def loadPremiumArticle(i):
+		a = requests.get(i['href'])
+		soup = BeautifulSoup(a.content, 'html.parser')
+		caption = soup.find('img', class_='lead-img')['title']
+		leadimg = soup.find('picture').find('source')['srcset']
+		body = soup.find('div',class_='articlebodycontent')
+		
+		children = []
+		for child in body.findChildren(recursive=False):
+		    el = child.find('div')
+		    if not el:
+		        children.append(str(child))
+		
+		st.title(i['title'])
+		st.image(leadimg, caption=caption, use_container_width=True)
+		st.write('\n'.join(children), unsafe_allow_html=True)
+
+	cols = st.columns(7, gap='small')
+	for i, item in enumerate(links):
+		c = cols[i%len(cols)]
+		img = c.image(item['img'].replace('SQUARE_80','SQUARE_140'), use_container_width=True)
+		if c.button(f"{item['title']}", use_container_width=True):
+			loadPremiumArticle(item)
